138e5df2983a7519ce9cb58eb56eb0e6b176b385
neonphog <neonphog@gmail.com>
2018-07-24 10:36:39 -0600

    move test code into examples

---
+++ examples/ipc/echo-client.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+'use strict'
+
+const crypto = require('crypto')
+const n3h = require('../../lib/index')
+
+async function _main () {
+  const cli = new n3h.ipc.Client('ipc://echo-server.sock')
+  cli.on('recvSend', (msg) => {
+    console.log('GOT RECV_SEND:',
+      msg.fromAddress.toString('hex'),
+      msg.data.toString())
+  })
+  cli.on('recvCall', (msg) => {
+    console.log('GOT RECV_CALL:',
+      msg.messageId.toString('hex'),
+      msg.fromAddress.toString('hex'),
+      msg.data.toString())
+  })
+
+  await cli.ready()
+
+  console.log('sending...')
+  await cli.send(Buffer.from('ab12', 'hex'), Buffer.from('hello'))
+  console.log('send success : )')
+
+  console.log('calling...')
+  const resp = await cli.call(
+    Buffer.from('ab12', 'hex'),
+    Buffer.from('hello')
+  )
+  console.log('call success:',
+    resp.fromAddress.toString('hex'),
+    resp.data.toString())
+
+  cli.close()
+}
+
+_main().then(() => {}, (err) => {
+  console.error(err)
+  process.exit(1)
+})
---
+++ examples/ipc/echo-server.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict'
+
+const n3h = require('../../lib/index')
+
+async function _main () {
+  const srv = new n3h.ipc.Server(['ipc://echo-server.sock'])
+
+  process.on('SIGINT', () => {
+    srv.close()
+    console.log('socket closed')
+    process.exit(0)
+  })
+
+  srv.on('send', (msg) => {
+    console.log('GOT SEND:', {
+      toAddress: msg.toAddress.toString('hex'),
+      data: msg.data.toString()
+    })
+    msg.resolve()
+
+    // for an echo, the fromAddress is the toAddress : )
+    srv.recvSend(
+      msg.toAddress,
+      Buffer.from("echo: " + msg.data.toString()))
+
+  })
+
+  srv.on('call', (msg) => {
+    console.log('GOT CALL:', {
+      messageId: msg.messageId.toString('hex'),
+      toAddress: msg.toAddress.toString('hex'),
+      data: msg.data.toString()
+    })
+    msg.resolve()
+
+    // for an echo, the fromAddress is the toAddress : )
+    srv.recvCall(
+      msg.messageId,
+      msg.toAddress,
+      Buffer.from("echo: " + msg.data.toString()))
+
+    // also simulate a response
+    srv.recvCallResp(
+      msg.messageId,
+      msg.toAddress,
+      Buffer.from("resp: " + msg.data.toString()))
+  })
+
+  await srv.ready()
+  console.log('up and running')
+}
+
+_main().then(() => {}, (err) => {
+  console.error(err)
+  process.exit(1)
+})
