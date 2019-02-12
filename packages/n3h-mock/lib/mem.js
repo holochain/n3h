08982504894827ce9b5d7a0ebb5f3105daf5f1a8
damien <doute.d@gmail.com>
2019-02-12 16:15:40 -0500

    WIP - untrack in n3hmock

--- packages/n3h-mock/lib/mem.js
+++ packages/n3h-mock/lib/mem.js
@@ -89,6 +89,15 @@ class Mem {
   /**
    *
    */
+  hasValue (data) {
+    const entry = getEntry(data)
+    return this.has(entry.hash)
+  }
+
+
+  /**
+   *
+   */
   has (hash) {
     const loc = getLoc(Buffer.from(hash, 'base64'))
     if (!(loc in this._data)) {
